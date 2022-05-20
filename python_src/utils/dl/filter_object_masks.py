4cededa0a16728847ba7f97afdaee8791738e25e
Saurabh Gupta <saurabh.gupta1002@gmail.com>
2022-05-20 03:52:14 +0200

    Detectron Instance Seg working

--- python_src/utils/dl/filter_object_masks.py
+++ python_src/utils/dl/filter_object_masks.py
@@ -12,25 +12,33 @@
 # ==============================================================================
 from typing import Tuple
 import numpy as np
-import cv2
 
 from tqdm import tqdm
 
-import os
 import sys
 sys.path.append("..")
 from dataloader import DatasetRGBD
 
 import torch
+TORCH_VERSION = ".".join(torch.__version__.split(".")[:2])
+CUDA_VERSION = torch.__version__.split("+")[-1]
+print(f"torch: {TORCH_VERSION}; cuda: {CUDA_VERSION}")
+
 import detectron2
 from detectron2.utils.logger import setup_logger
 setup_logger()
+
+import os, cv2
+
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
-
+from detectron2.projects.panoptic_deeplab import (
+    PanopticDeeplabDatasetMapper,
+    add_panoptic_deeplab_config,
+)
 
 def near_edge(mask: np.ndarray, edge_threshold) -> bool:
     """Check if the prediction mask is near the image boundary
@@ -52,6 +60,7 @@ def filter_object_masks(
     rgb_frame_path: str,
     depth_frame_path: str,
     predictor,
+    class_names: str,
     edge_threshold: int,
     min_mask_area: int,
 ) -> Tuple[list, list, list]:
@@ -71,53 +80,62 @@ def filter_object_masks(
     - filtered_masks: Object masks that pass the filtering
     """
     im = cv2.imread(rgb_frame_path)
-    cv2.imshow("image", im)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
     outputs = predictor(im)
-    # print(outputs)
+    predictions = outputs["instances"].to("cpu")
+    boxes = predictions.pred_boxes.tensor.cpu().numpy() if predictions.has("pred_boxes") else None
+    scores = predictions.scores.numpy() if predictions.has("scores") else None
+    
+    classes = predictions.pred_classes.tolist() if predictions.has("pred_classes") else None
+    labels = np.array([class_names[i] for i in classes])
+
+    if predictions.has("pred_masks"):
+        masks = np.asarray(predictions.pred_masks)
+    else:
+        masks = None
+    
     masks_path = os.path.splitext(rgb_frame_path)[0]
     depth_image = cv2.imread(depth_frame_path, cv2.CV_16UC1)
-    # try:
-    #     os.mkdir(masks_path)
-    #     os.mkdir(masks_path + '/masks')
-    # except FileExistsError:
-    #     pass
-    # np.savetxt(masks_path + "/class_labels.txt", pred_cls, fmt="%s")
-    # np.savetxt(masks_path + "/bboxes.txt", boxes.reshape(-1, 4))
-    # for i, mask in enumerate(masks):
-    #     cv2.imwrite(masks_path + '/masks/' + str(i) + '.png', mask * depth_image)
+    try:
+        os.mkdir(masks_path)
+        os.mkdir(masks_path + '/masks')
+    except FileExistsError:
+        pass
+    np.savetxt(masks_path + "/class_labels.txt", labels, fmt="%s")
+    np.savetxt(masks_path + "/bboxes.txt", boxes)
+    for i, mask in enumerate(masks):
+        cv2.imwrite(masks_path + '/masks/' + str(i) + '.png', mask * depth_image)
 
     filtered_predictions = []
     filtered_masks = []
     filtered_boxes = []
 
-    # for pred, mask, box in zip(pred_cls, masks, boxes):
-    #     if (not near_edge(mask, edge_threshold)) and (np.sum(mask) >= min_mask_area):
-    #         filtered_masks.append(mask)
-    #         filtered_boxes.append(box)
-    #         filtered_predictions.append(pred)
+    for pred, mask, box in zip(labels, masks, boxes):
+        if (not near_edge(mask, edge_threshold)) and (np.sum(mask) >= min_mask_area):
+            filtered_masks.append(mask)
+            filtered_boxes.append(box)
+            filtered_predictions.append(pred)
 
     return filtered_predictions, filtered_boxes, filtered_masks
 
+def main():
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.9
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+    class_names = MetadataCatalog.get(cfg.DATASETS.TRAIN[0]).get("thing_classes", None)
 
-cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.9
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
-predictor = DefaultPredictor(cfg)
-datadir = "../../../datasets/rgbd_dataset_freiburg1_desk/"
+    predictor = DefaultPredictor(cfg)
+    IMAGE_DIR = "../../../datasets/rgbd_dataset_freiburg1_desk/"
 
-rgbd_data = DatasetRGBD(datadir)
+    rgbd_data = DatasetRGBD(IMAGE_DIR)
 
-edge_threshold = 20
-min_mask_area = 50 * 50
-for n, [_, rgb_frame, _] in enumerate(rgbd_data):
-    print("Hi")
-    outputs = predictor(rgb_frame)
+    edge_threshold = 20
+    min_mask_area = 50 * 50
+    for n in range(len(rgbd_data)):
+        # masks(n, 480, 640), boxes(n, 4), pred_cls(n,)
+        pred_cls, boxes, masks = filter_object_masks(
+            rgbd_data.rgb_paths[n], rgbd_data.depth_paths[n], predictor, class_names, edge_threshold, min_mask_area
+        )
 
-    # masks(n, 480, 640), boxes(n, 2, 2), pred_cls(n,)
-    # pred_cls, boxes, masks = filter_object_masks(
-    #     rgbd_data.rgb_paths[n], rgbd_data.depth_paths[n], predictor, edge_threshold, min_mask_area
-    # )
-    # show_object_masks(rgb_frame, pred_cls, boxes, masks)
+if __name__=='__main__':
+    main()
