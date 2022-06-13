800a7509b53d99dbcc7bbb76704ae3240d7f6dd8
Saurabh Gupta <saurabh.gupta1002@gmail.com>
2022-06-13 19:54:21 +0200

    Update dataloader for tensor queries

--- c++_src/dataloaders/freiburg1_desk/freiburg1_desk.cpp
+++ c++_src/dataloaders/freiburg1_desk/freiburg1_desk.cpp
@@ -65,6 +65,10 @@ readData(const fs::path& dataset_path, int n_scans) {
 }  // namespace
 
 namespace datasets {
+using Image = o3d::geometry::Image;
+using Image_t = o3d::t::geometry::Image;
+auto gpu = o3d::core::Device("CUDA:0,1,2,3");
+
 freiburg1_desk::freiburg1_desk(const std::string& data_root_dir,
                                const YAML::Node& cfg,
                                int n_scans)
@@ -75,9 +79,8 @@ freiburg1_desk::freiburg1_desk(const std::string& data_root_dir,
             readData(freiburg_root_dir_, n_scans);
 }
 
-using Image = o3d::geometry::Image;
-std::tuple<double, Eigen::Vector<double, 7>, Image, Image>
-freiburg1_desk::operator[](int idx) const {
+std::tuple<double, Eigen::Vector<double, 7>, Image, Image> freiburg1_desk::At(
+        int idx) const {
     double timestamp = time_[idx];
     Eigen::Vector<double, 7> pose = poses_[idx];
     Image rgb_image_8bit;
@@ -92,24 +95,22 @@ freiburg1_desk::operator[](int idx) const {
     exit(1);
 }
 
-using Image_t = o3d::t::geometry::Image;
 std::tuple<double, Eigen::Vector<double, 7>, Image_t, Image_t>
-freiburg1_desk_t::operator[](int idx) const {
+freiburg1_desk::At_t(int idx) const {
     double timestamp = time_[idx];
     Eigen::Vector<double, 7> pose = poses_[idx];
-    Image_t rgb_image_8bit;
-    Image_t depth_image_16bit;
-    return std::make_tuple(timestamp, pose, rgb_image_8bit, depth_image_16bit);
-}
-
-freiburg1_desk_t::freiburg1_desk_t(const std::string& data_root_dir,
-                                   const YAML::Node& cfg,
-                                   int n_scans)
-    : cfg_(cfg) {
-    auto freiburg_root_dir_ = fs::absolute(fs::path(data_root_dir));
-
-    std::tie(time_, poses_, rgb_files_, depth_files_) =
-            readData(freiburg_root_dir_, n_scans);
+    Image rgb_image_8bit;
+    Image depth_image_16bit;
+    if (open3d::io::ReadImage(rgb_files_[idx], rgb_image_8bit) &&
+        open3d::io::ReadImage(depth_files_[idx], depth_image_16bit)) {
+        auto rgb_t = o3d::t::geometry::Image::FromLegacy(rgb_image_8bit, gpu);
+        auto depth_t =
+                o3d::t::geometry::Image::FromLegacy(depth_image_16bit, gpu);
+        return std::make_tuple(timestamp, pose, rgb_t, depth_t);
+    }
+    o3d::utility::LogWarning("Failed to read following files:\n{}\n{}",
+                             rgb_files_[idx], depth_files_[idx]);
+    exit(1);
 }
 
 maskrcnn::maskrcnn(freiburg1_desk data_fr1) : data_fr1_(std::move(data_fr1)) {}
@@ -117,7 +118,7 @@ std::tuple<std::vector<std::string>,
            std::vector<float>,
            std::vector<std::vector<int>>,
            std::vector<Image>>
-maskrcnn::operator[](int idx) const {
+maskrcnn::At(int idx) const {
     const auto& masks_path = fs::path(data_fr1_.rgb_files_[idx]).parent_path() /
                              fs::path(data_fr1_.rgb_files_[idx]).stem();
 
@@ -152,6 +153,49 @@ maskrcnn::operator[](int idx) const {
         Image mask;
         o3d::io::ReadImage(image, mask);
         masks.emplace_back(mask);
+    }
+    return std::make_tuple(class_labels, class_scores, bboxes, masks);
+}
+
+std::tuple<std::vector<std::string>,
+           std::vector<float>,
+           std::vector<std::vector<int>>,
+           std::vector<Image_t>>
+maskrcnn::At_t(int idx) const {
+    const auto& masks_path = fs::path(data_fr1_.rgb_files_[idx]).parent_path() /
+                             fs::path(data_fr1_.rgb_files_[idx]).stem();
+
+    std::string class_label;
+    std::vector<std::string> class_labels;
+    std::ifstream labels_file(masks_path / "class_labels.txt",
+                              std::ios_base::in);
+    while (std::getline(labels_file, class_label)) {
+        class_labels.emplace_back(class_label);
+    }
+
+    float score;
+    std::vector<float> class_scores;
+    std::ifstream scores_file(masks_path / "scores.txt", std::ios_base::in);
+    while (scores_file >> score) {
+        class_scores.emplace_back(score);
+    }
+
+    double tlx, tly, brx, bry;
+    std::vector<std::vector<int>> bboxes;
+    std::ifstream bboxes_file(masks_path / "bboxes.txt", std::ios_base::in);
+    while (bboxes_file >> tlx >> tly >> brx >> bry) {
+        bboxes.emplace_back(
+                std::vector<int>{static_cast<int>(tlx), static_cast<int>(tly),
+                                 static_cast<int>(brx), static_cast<int>(bry)});
+    }
+
+    std::vector<Image_t> masks;
+    for (size_t i = 0; i < class_labels.size(); i++) {
+        const auto& image =
+                masks_path.string() + "/masks/" + std::to_string(i) + ".png";
+        Image mask;
+        o3d::io::ReadImage(image, mask);
+        masks.emplace_back(o3d::t::geometry::Image::FromLegacy(mask, gpu));
     }
     return std::make_tuple(class_labels, class_scores, bboxes, masks);
 }
